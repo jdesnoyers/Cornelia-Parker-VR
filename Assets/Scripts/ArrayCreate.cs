389c701957d81ffa58da6e79b6125ecc13222f35
jdesnoyers <johnny.desnoyers@gmail.com>
2017-02-14 23:35:08 -0600

    Add files via upload

---
+++ Assets/Scripts/ArrayCreate.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayCreate : MonoBehaviour {
+
+    [SerializeField] private Rigidbody arrayOriginal;
+    [SerializeField] private Transform arrayParent;
+
+    public int arraySizeX; //number of instances in X
+    public int arraySizeY; //number of instances in Y
+    public int arraySizeZ; //number of instances in Z
+    public float arraySpacing; //spacing between instances in all directions
+    public float arrayRandomize; //randomize positions
+    public float arrayAngle; //add slope to array
+    public static bool arrayGravAct = false; //boolean to activate/deactivate local gravity
+    public float gravityConstantSet = 6.674E-11f;
+    public static float gravityConstant = 6.674E-11f;
+
+    [HideInInspector] public static float totalMass; //stores total mass of array
+    [HideInInspector] public static int arraySizeN; //stores total size of array
+    [HideInInspector] public static GameObject[] arrayObjects = null; //array of objets with tag "arrayObject"
+    [HideInInspector] public static Vector3 arrayCOM; //array center of mass
+
+    // Use this for initialization
+    void Start () {      
+
+        for (int i = 0; i < arraySizeX; i++)
+        {
+            for (int j = 0; j < arraySizeY; j++)
+            {
+                for(int k = 0; k < arraySizeZ; k++)
+                {
+                    Vector3 arrayTranslate = new Vector3(i+(Random.Range(-1.0f,1.0f)*arrayRandomize), j + (Random.Range(-1.0f, 1.0f) * arrayRandomize)+ (i*arrayAngle), k + (Random.Range(-1.0f, 1.0f) * arrayRandomize) + (i * arrayAngle));
+                    Instantiate(arrayOriginal,arrayParent.position + (arrayTranslate * arraySpacing),arrayOriginal.transform.rotation, arrayParent);
+                }
+                
+            }
+        }
+
+        //define array of objects
+        arrayObjects = GameObject.FindGameObjectsWithTag("arrayObject");
+
+        //calculate total mass
+        foreach (GameObject arrayObject in arrayObjects)
+            totalMass += arrayObject.GetComponent<Rigidbody>().mass;
+
+        arraySizeN = arrayObjects.Length;
+        gravityConstant = gravityConstantSet;
+            
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        Vector3 sumCOM = Vector3.zero; //create/reset vector3
+
+        // calculate center of mass
+        foreach (GameObject arrayObject in arrayObjects)
+        {
+            
+            sumCOM += arrayObject.GetComponent<Rigidbody>().worldCenterOfMass;
+        }
+
+        arrayCOM = sumCOM / arraySizeN; //calculate average
+
+    }
+}
---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VR = UnityEngine.VR;
+
+public class ControllerInput : MonoBehaviour {
+
+    public float viscosityHigh = 1.0f;
+    public float frictionHigh = 0.6f;
+    public float frictionLow = 0.1f;
+    public bool viscosityOn = false;
+    private OVRPlayerController playerController;
+    public GameObject sunObject;
+    [SerializeField] private Toggle gravityToggle;
+    [SerializeField] private Toggle swarmToggle;
+    [SerializeField] private Toggle viscosityToggle;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Awake()
+    {
+        //get controller
+        OVRPlayerController[] playerControllers;
+        playerControllers = gameObject.GetComponentsInChildren<OVRPlayerController>();
+
+        if (playerControllers.Length == 0)
+        {
+            Debug.LogWarning("OVRMainMenu: No OVRPlayerController attached.");
+        }
+        else if (playerControllers.Length > 1)
+        {
+            Debug.LogWarning("OVRMainMenu: More then 1 OVRPlayerController attached.");
+        }
+        else
+        {
+            playerController = playerControllers[0];
+        }
+
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.X) || Input.GetButtonDown("Jump")) playerController.Jump();
+
+        //Turn on/off "true" gravity
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetButtonDown("Gravity"))
+        {
+            foreach (GameObject arrayObject in ArrayCreate.arrayObjects)
+            {
+                arrayObject.GetComponent<Rigidbody>().useGravity = !arrayObject.GetComponent<Rigidbody>().useGravity;
+            }
+
+            gravityToggle.isOn = !gravityToggle.isOn;
+
+        }
+
+        //Turn on/off "swarm" gravity
+        if (Input.GetKeyDown(KeyCode.C) || Input.GetButtonDown("Swarm"))
+        {
+            ArrayCreate.arrayGravAct = !ArrayCreate.arrayGravAct;
+            swarmToggle.isOn = !swarmToggle.isOn;
+        }
+
+        if (Input.GetKey(KeyCode.T) || Input.GetButton("Time"))
+        {
+            sunObject.GetComponent<SunMovement>().MoveSun();
+        }
+
+        //Turn on/off object viscosity
+        if (Input.GetKeyDown(KeyCode.V) || Input.GetButtonDown("Viscosity"))
+        {
+            viscosityOn = !viscosityOn;
+
+            viscosityToggle.isOn = !viscosityToggle.isOn;
+
+            if (viscosityOn == true)
+            {
+                foreach (GameObject arrayObject in ArrayCreate.arrayObjects)
+                {
+                    arrayObject.GetComponent<Rigidbody>().drag = viscosityHigh;
+                    arrayObject.GetComponent<Rigidbody>().angularDrag = viscosityHigh;
+                    arrayObject.GetComponent<BoxCollider>().material.staticFriction = frictionHigh;
+                    arrayObject.GetComponent<BoxCollider>().material.dynamicFriction = frictionHigh;
+                }
+            }
+            else
+            {
+                foreach (GameObject arrayObject in ArrayCreate.arrayObjects)
+                {
+                    arrayObject.GetComponent<Rigidbody>().drag = 0;
+                    arrayObject.GetComponent<Rigidbody>().angularDrag = 0;
+                    arrayObject.GetComponent<BoxCollider>().material.staticFriction = frictionLow;
+                    arrayObject.GetComponent<BoxCollider>().material.dynamicFriction = frictionLow;
+                }
+            }
+        }
+    }
+}
